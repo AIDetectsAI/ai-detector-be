ec58b30d20f939d96b1f9c80ba5515a0ad3fa378
Szymon <stszymek@gmail.com>
2025-06-18 23:59:01 +0200

    Initial commit

---
+++ src/main/java/org/example/aidetectorbe/AiDetectorBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.aidetectorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDetectorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDetectorBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aidetectorbe/AiDetectorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aidetectorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDetectorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
