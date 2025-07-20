450c1d09fbfb428b4f500bb68ac8182cdc575807
Szymon <stszymek@gmail.com>
2025-07-20 17:32:44 +0200

    added another logger

--- src/main/java/org/example/aidetectorbe/logger/Log.java
+++ src/main/java/org/example/aidetectorbe/logger/Log.java
@@ -7,13 +7,17 @@ public class Log {
 
     private static final Logger logger = LogManager.getLogger("GlobalLogger");
 
-    public static void info(String message) {
-        logger.info(message);
-    }
+    public static void info(String message) {logger.info(message);}
 
-    public static void error(String message) {
-        logger.error(message);
-    }
+    public static void error(String message) {logger.error(message);}
+
+    public static void debug(String message) {logger.debug(message);}
+
+    public static void warn(String message) {logger.warn(message);}
+
+    public static void fatal(String message) {logger.fatal(message);}
+
+    public static void verbose(String message) {logger.trace(message);}
 
     private Log() {
         // Private constructor to prevent instantiation
