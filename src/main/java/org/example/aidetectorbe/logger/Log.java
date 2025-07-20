1a3e4e81622f3f476fea680bdebccc833054d684
Szymon <stszymek@gmail.com>
2025-07-20 16:51:47 +0200

    comments resolved

--- src/main/java/org/example/aidetectorbe/logger/Log.java
+++ src/main/java/org/example/aidetectorbe/logger/Log.java
@@ -1,11 +1,10 @@
 package org.example.aidetectorbe.logger;
 
-import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log {
-    @Getter
+
     private static final Logger logger = LogManager.getLogger("GlobalLogger");
 
     public static void info(String message) {
