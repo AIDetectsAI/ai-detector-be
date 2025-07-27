9ad589560c3b01af59b2d659b97358281a917e03
jarzeckil <jarzeckil@gmail.com>
2025-07-27 17:00:47 +0200

    added password hashing while saving

--- src/main/java/org/example/aidetectorbe/services/UserService.java
+++ src/main/java/org/example/aidetectorbe/services/UserService.java
@@ -3,6 +3,7 @@ package org.example.aidetectorbe.services;
 import lombok.AllArgsConstructor;
 import org.example.aidetectorbe.dto.UserDTO;
 import org.example.aidetectorbe.entities.User;
+import org.example.aidetectorbe.logger.Log;
 import org.example.aidetectorbe.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,12 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordHasher passwordHasher;
 
     public UUID createUser(UserDTO userDTO) {
-
-        User user = new User(userDTO.getLogin(), userDTO.getPassword(), userDTO.getEmail());
+        User user = new User(userDTO.getLogin(), passwordHasher.hashPassword(userDTO.getPassword()), userDTO.getEmail());
         userRepository.save(user);
+        Log.info("Saved user with id " + user.getId());
         return user.getId();
     }
 }
