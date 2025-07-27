fb5f3c94293f6accc6fb791f77043ee69d0837c2
Janek0337 <artyleriapolska@gmail.com>
2025-07-27 20:35:48 +0200

    Implemented login endpoint

--- src/main/java/org/example/aidetectorbe/controllers/UserController.java
+++ src/main/java/org/example/aidetectorbe/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.example.aidetectorbe.controllers;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.example.aidetectorbe.dto.UserDTO;
+import org.example.aidetectorbe.entities.User;
 import org.example.aidetectorbe.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.example.aidetectorbe.security.*;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -33,4 +36,17 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userId);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody UserDTO xUser){
+        Optional<User> userOptional = userService.getUserByLogin(xUser.getLogin());
+        if(userOptional.isEmpty()) {
+            return ResponseEntity.status(401).body("Login does not exist");
+        }
+        User user = userOptional.get();
+        if(!userService.encryptPassword(xUser.getPassword()).equals(user.getPassword())){
+            return ResponseEntity.status(401).body("Invalid password");
+        }
+        String token = userService.generateToken(xUser.getLogin());
+        return ResponseEntity.ok(Map.of("token", token));
+    }
 }
--- src/main/java/org/example/aidetectorbe/services/UserService.java
+++ src/main/java/org/example/aidetectorbe/services/UserService.java
@@ -4,20 +4,35 @@ import lombok.AllArgsConstructor;
 import org.example.aidetectorbe.dto.UserDTO;
 import org.example.aidetectorbe.entities.User;
 import org.example.aidetectorbe.repository.UserRepository;
+import org.example.aidetectorbe.security.JwtUtil;
 import org.springframework.stereotype.Service;
-
+import org.example.aidetectorbe.security.Encoder;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final Encoder encoder;
 
     public UUID createUser(UserDTO userDTO) {
 
         User user = new User(userDTO.getLogin(), userDTO.getPassword(), userDTO.getEmail());
         userRepository.save(user);
         return user.getId();
+    }
+
+    public String generateToken(String login){
+        return jwtUtil.generateToken(login);
+    }
+
+    public Optional<User> getUserByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+
+    public String encryptPassword(String password){
+        return encoder.encryptPassword(password);
     }
 }
