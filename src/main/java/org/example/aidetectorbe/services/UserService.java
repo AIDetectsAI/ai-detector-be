ba1110525a355486d7a669c558f758ea55cac25c
jarzeckil <jarzeckil@gmail.com>
2025-07-28 19:27:12 +0200

    got rid of redundant logging

--- src/main/java/org/example/aidetectorbe/services/UserService.java
+++ src/main/java/org/example/aidetectorbe/services/UserService.java
@@ -3,7 +3,6 @@ package org.example.aidetectorbe.services;
 import lombok.AllArgsConstructor;
 import org.example.aidetectorbe.dto.UserDTO;
 import org.example.aidetectorbe.entities.User;
-import org.example.aidetectorbe.logger.Log;
 import org.example.aidetectorbe.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +18,6 @@ public class UserService {
     public UUID createUser(UserDTO userDTO) {
         User user = new User(userDTO.getLogin(), passwordHasher.hashPassword(userDTO.getPassword()), userDTO.getEmail());
         userRepository.save(user);
-        Log.info("Saved user with id " + user.getId());
         return user.getId();
     }
 }
