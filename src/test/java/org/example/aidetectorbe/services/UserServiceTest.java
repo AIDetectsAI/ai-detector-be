3fb79bf4241431d4e1e85347fd38949be90ceb31
jarzeckil <jarzeckil@gmail.com>
2025-07-28 19:26:54 +0200

    space

--- src/test/java/org/example/aidetectorbe/services/UserServiceTest.java
+++ src/test/java/org/example/aidetectorbe/services/UserServiceTest.java
@@ -8,7 +8,6 @@ import org.mockito.ArgumentCaptor;
 
 import java.util.UUID;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
