89b7eb797bbcc467c0d4d817379b63eb5268f5b3
Piotr Sosnowski <piotrsosnowski@autograf.pl>
2026-04-22 14:59:48 +0200

    Added new query service tests for deleting all queries of a user

--- src/test/java/org/example/aidetectorbe/services/QueryServiceImplTest.java
+++ src/test/java/org/example/aidetectorbe/services/QueryServiceImplTest.java
@@ -5,10 +5,13 @@ import org.example.aidetectorbe.repository.QueryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.springframework.security.core.userdetails.User;
 
 import jakarta.persistence.EntityNotFoundException;
 
 import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -80,6 +83,53 @@ class QueryServiceImplTest {
                 queryService.deleteQuery(queryId, attackerLogin));
 
         assertEquals("Query was not found", exception.getMessage());
+        verify(mockQueryRepository, never()).save(any(Query.class));
+    }
+
+    @Test
+    void deleteAllQueries_GivenExistingQueries_ShouldMarkAsDeleted() {
+        // Arrange
+        Long queryId1 = 1L;
+        Long queryId2 = 2L;
+        String userLogin = "testUser";
+        Query query1 = new Query();
+        query1.setId(queryId1);
+        query1.setIsDeleted(false);
+        Query query2 = new Query();
+        query2.setId(queryId2);
+        query2.setIsDeleted(false);
+
+        Set<Query> queries = new HashSet<>();
+        queries.add(query1);
+        queries.add(query2);
+        when(mockQueryRepository.findByUser_Login(userLogin))
+                .thenReturn(Optional.of(queries));
+
+        // Act
+        queryService.deleteAllQueries(userLogin);
+
+        // Assert
+        ArgumentCaptor<Query> queryCaptor = ArgumentCaptor.forClass(Query.class);
+        verify(mockQueryRepository).save(queryCaptor.capture());
+        Query savedQuery = queryCaptor.getValue();
+
+        assertTrue(savedQuery.getIsDeleted());
+        verify(mockQueryRepository).findByUser_Login(userLogin);
+    }
+
+    @Test
+    void deleteAllQueries_GivenNonExistingUser_ShouldThrowEntityNotFoundException() {
+        // Arrange
+        String userLogin = "testUser";
+
+        when(mockQueryRepository.findByUser_Login(userLogin))
+                .thenReturn(Optional.empty());
+
+        // Act & Assert
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () ->
+                queryService.deleteAllQueries(userLogin));
+
+        assertEquals("Queries were not found", exception.getMessage());
         verify(mockQueryRepository, never()).save(any(Query.class));
     }
 
