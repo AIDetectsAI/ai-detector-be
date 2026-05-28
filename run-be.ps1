78fa526f0dc5d6578ca62ddde9ef6dd7d2739f4f
Lukasz004 <01187549@pw.edu.pl>
2026-05-28 16:32:02 +0200

    updated shell scripts to run be

--- run-be.ps1
+++ run-be.ps1
@@ -1,4 +1,4 @@
 mvn clean package
 docker rm -f "ai-detector-be" 2>$null
 docker build -t ai-detector-be .
-docker run -d --name "ai-detector-be" -p 8081:8081 --add-host host.docker.internal:host-gateway -e SPRING_DATASOURCE_URL="jdbc:postgresql://host.docker.internal:15432/AIDB" -e AI_SERVICE_URL="http://host.docker.internal:8000" ai-detector-be
+docker run -d --name "ai-detector-be" -p 8081:8081 --add-host host.docker.internal:host-gateway --env-file .env -e SPRING_DATASOURCE_URL="jdbc:postgresql://host.docker.internal:15432/AIDB" -e AI_SERVICE_URL="http://host.docker.internal:8000" ai-detector-be
